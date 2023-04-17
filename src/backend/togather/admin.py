0a19fd25f2acf8eec843061797159023d3033b44
Jakub Wilk <wilkjakub64@gmail.com>
2023-04-17 23:03:25 +0200

    Clean up models

--- src/backend/togather/admin.py
+++ src/backend/togather/admin.py
@@ -2,7 +2,47 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from togather.models import TogatherUser
+from togather.models import TogatherUser, Event
+
+
+class CreatedEventsInline(admin.TabularInline):
+    model = Event
+    show_change_link = True
+    fields = ["name"]
+    verbose_name_plural = "Created Events"
+    extra = 0
+
+
+class ModeratedEventsInline(admin.TabularInline):
+    model = Event.moderators.through
+    verbose_name_plural = "Moderated Events"
+    extra = 0
+
+
+class ParticipatedEventsInline(admin.TabularInline):
+    model = Event.participants.through
+    verbose_name_plural = "Participated-in Events"
+    extra = 0
+
+
+class InterestedEventsInline(admin.TabularInline):
+    model = Event.interested.through
+    verbose_name_plural = "Interested-in Events"
+    extra = 0
+
+
+class RequestedEventsInline(admin.TabularInline):
+    model = Event.participation_requests.through
+    verbose_name_plural = "Requested-access-to Events"
+    extra = 0
+
+
+class TogatherUserAdmin(admin.ModelAdmin):
+    model = TogatherUser
+    inlines = [CreatedEventsInline, ModeratedEventsInline, ParticipatedEventsInline, InterestedEventsInline, RequestedEventsInline]
+
+
+admin.site.register(TogatherUser, TogatherUserAdmin)
 
 
 class TogatherUserInline(admin.StackedInline):
@@ -16,3 +56,5 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Event)
--- src/backend/togather/models.py
+++ src/backend/togather/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+
+class TogatherUser(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="togather_user")
+    phone_number = PhoneNumberField(blank=True)  # TODO change before release to disallow blank
+    bio = models.TextField(blank=True)
+    other_sites = models.JSONField(blank=True, null=True, default=list)
+    friends = models.ManyToManyField("self", blank=True)
+    creator_score = models.PositiveSmallIntegerField(blank=True, null=True)
+    participant_score = models.PositiveSmallIntegerField(blank=True, null=True)
+
 
 class Event(models.Model):
     # General
@@ -21,12 +35,12 @@ class Event(models.Model):
     access_type = models.PositiveSmallIntegerField(choices=AccessTypes.choices)
 
     # Users
-    # creator = models.ForeignKey(TogatherUser)
-    # moderators = models.ManyToManyField(TogatherUser, blank=True)
-    # participants = models.ManyToManyField(TogatherUser, blank=True)
-    # interested = models.ManyToManyField(TogatherUser, blank=True)
-    # participation_requests = models.ManyToManyField(TogatherUser)
-    max_participants = models.PositiveIntegerField()
+    creator = models.ForeignKey(TogatherUser, on_delete=models.DO_NOTHING, blank=True, null=True, related_name="created_events")
+    moderators = models.ManyToManyField(TogatherUser, blank=True, related_name="moderated_events")
+    participants = models.ManyToManyField(TogatherUser, blank=True, related_name="participating_events")
+    interested = models.ManyToManyField(TogatherUser, blank=True, related_name="interested_events")
+    participation_requests = models.ManyToManyField(TogatherUser, blank=True, related_name="requested_events")
+    max_participants = models.PositiveIntegerField(blank=True, null=True)
 
     # Location
     geo_lat = models.IntegerField()
@@ -41,6 +55,13 @@ class Event(models.Model):
     is_archived = models.BooleanField(default=False)
 
 
+# If the event is archived, it doesn't get deleted when the creator deletes their profile
+@receiver(pre_delete, sender=TogatherUser, dispatch_uid="togatheruser_delete_signal")
+def delete_if_not_archived(sender, instance, using, **kwargs):
+    instance.created_events.filter(is_archived=True).update(creator=None)
+    instance.created_events.filter(is_archived=False).delete()
+
+
 class Photo(models.Model):
     photo = models.ImageField()
     order = models.PositiveSmallIntegerField(unique=True)
@@ -50,19 +71,8 @@ class Photo(models.Model):
 
 
 class UserPhoto(Photo):
-    pass
+    user = models.ForeignKey(TogatherUser, on_delete=models.CASCADE, related_name="photos")
 
 
 class EventPhoto(Photo):
-    pass
-
-
-class TogatherUser(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = PhoneNumberField()
-    bio = models.TextField()
-    other_sites = models.JSONField()
-    friends = models.ManyToManyField("self")
-    creator_score = models.PositiveSmallIntegerField()
-    participant_score = models.PositiveSmallIntegerField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="photos")
